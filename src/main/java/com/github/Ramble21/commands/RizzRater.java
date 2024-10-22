b82789f375b4a02dc1d5123416a75a1a2dc78e16
Ramble21 <zramble21@gmail.com>
2024-10-21 21:47:31 -0400

    added seeds for grabip and rizzrater

--- src/main/java/com/github/Ramble21/commands/RizzRater.java
+++ src/main/java/com/github/Ramble21/commands/RizzRater.java
@@ -1,14 +1,10 @@
 package com.github.Ramble21.commands;
 
-import com.github.Ramble21.classes.Quote;
 import com.github.Ramble21.classes.Ramble21;
 import com.github.Ramble21.command.Command;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
-import java.awt.*;
 import java.util.Objects;
 
 public class RizzRater implements Command {
@@ -16,9 +12,13 @@ public class RizzRater implements Command {
     public void execute(SlashCommandInteractionEvent event){
         User skibidi = Objects.requireNonNull(event.getOption("member")).getAsUser();
         String user = skibidi.getId();
+        System.out.println(user);
+
+        int seed = Ramble21.generateSeed(user);
+        int ballzakz = Ramble21.generateRizz(seed);
+
         user = "<@" + user + ">";
 
-        int ballzakz = (int)(1+Math.random()*10);
         event.reply(user + "'s " + Ramble21.rateRizz(ballzakz)).queue();
     }
 }
