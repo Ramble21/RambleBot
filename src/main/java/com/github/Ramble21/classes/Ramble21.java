c16e9f1b29b717d872af5d4c10c69a658993d5a6
Ramble21 <zramble21@gmail.com>
2024-10-21 23:39:25 -0500

    Use switch

--- src/main/java/com/github/Ramble21/classes/Ramble21.java
+++ src/main/java/com/github/Ramble21/classes/Ramble21.java
@@ -8,38 +8,31 @@ import java.util.Random;
 
 public class Ramble21 {
 
-    public static String rateRizz(int rizz){
-        if (rizz == 1){
-            return "rizz count is 1/10. You're playing Minecraft, in a cave, looking for diamonds";
+    public static String rateRizz(int rizz) {
+        switch (rizz) {
+            case 1:
+                return "rizz count is 1/10. You're playing Minecraft, in a cave, looking for diamonds";
+            case 2:
+                return "rizz count is 2/10. Maybe they should stop watching so much anime and touch some grass";
+            case 3:
+                return "rizz count is 3/10. It would've been higher if they didn't piss their pants every time they see their crush";\
+            case 4:
+                return "rizz count is 4/10. Womp womp";
+            case 5:
+                return "rizz count is 5/10. Literal NPC";
+            case 6:
+                return "rizz count is 6/10. I'm honestly shocked that its even this high";
+            case 7:
+                return "rizz count is 7/10. They only know how to rizz up dudes though, maybe they should try to vary it up";
+            case 8:
+                return "rizz count is 8/10. While getting dates is easy for them, their 1 inch penis makes it hard for anyone to want to stay with them for long";
+            case 9:
+                return "rizz count is 9/10. Maybe this is a result of them finally not playing geometry dash anymore";
+            case 10:
+                return "rizz count is 10/10. Holy shit. We have found the skibidi ohio rizzler themself. Maybe even the future CEO of Ohio";
+            default:
+                return "rizz count is -1/12 because there's a bug in ur code";
         }
-        else if (rizz == 2){
-            return "rizz count is 2/10. Maybe they should stop watching so much anime and touch some grass";
-        }
-        else if (rizz == 3){
-            return "rizz count is 3/10. It would've been higher if they didn't piss their pants every time they see their crush";
-        }
-        else if (rizz == 4){
-            return "rizz count is 4/10. Womp womp";
-        }
-        else if (rizz == 5){
-            return "rizz count is 5/10. Literal NPC";
-        }
-        else if (rizz == 6){
-            return "rizz count is 6/10. I'm honestly shocked that its even this high";
-        }
-        else if (rizz == 7){
-            return "rizz count is 7/10. They only know how to rizz up dudes though, maybe they should try to vary it up";
-        }
-        else if (rizz == 8){
-            return "rizz count is 8/10. While getting dates is easy for them, their 1 inch penis makes it hard for anyone to want to stay with them for long";
-        }
-        else if (rizz == 9){
-            return "rizz count is 9/10. Maybe this is a result of them finally not playing geometry dash anymore";
-        }
-        else if (rizz == 10){
-            return "rizz count is 10/10. Holy shit. We have found the skibidi ohio rizzler themself. Maybe even the future CEO of Ohio";
-        }
-        return "rizz count is -1/12 because there's a bug in ur code";
     }
 
     public static boolean isBrainrotServer(Guild guild){
