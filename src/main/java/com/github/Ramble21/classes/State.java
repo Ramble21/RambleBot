7180be20734249a1f85cdce9b338f14b69ab00d3
Ramble21 <zramble21@gmail.com>
2024-10-22 21:10:29 -0400

    state-flags command wip might be bugs

---
+++ src/main/java/com/github/Ramble21/classes/State.java
@@ -0,0 +1,67 @@
+package com.github.Ramble21.classes;
+
+import javax.swing.*;
+
+public class State {
+    private final String name;
+    private final String nameSnakeCase;
+    private final String capital;
+
+    public State(){
+
+        String[] states = {
+                "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
+                "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho",
+                "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
+                "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
+                "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+                "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
+                "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+                "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+                "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+        };
+
+        String[] statesSnakeCase = {
+                "alabama", "alaska", "arizona", "arkansas", "california", "colorado",
+                "connecticut", "delaware", "florida", "georgia", "hawaii", "idaho",
+                "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana",
+                "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi",
+                "missouri", "montana", "nebraska", "nevada", "new_hampshire",
+                "new_jersey", "new_mexico", "new_york", "north_carolina", "north_dakota",
+                "ohio", "oklahoma", "oregon", "pennsylvania", "rhode_island",
+                "south_carolina", "south_dakota", "tennessee", "texas", "utah",
+                "vermont", "virginia", "washington", "west_virginia", "wisconsin", "wyoming"
+        };
+
+
+        String[] capitals = {
+                "Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford",
+                "Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis",
+                "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston",
+                "Lansing", "Saint Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City",
+                "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus",
+                "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville",
+                "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison",
+                "Cheyenne"
+        };
+
+        int randomNo = (int)(Math.random()*50);
+        name = states[randomNo];
+        nameSnakeCase = statesSnakeCase[randomNo];
+        capital = capitals[randomNo];
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getNameSnakeCase(){
+        return nameSnakeCase;
+    }
+    public String getFlagLocation(){
+        return "src/main/images/flags/" + nameSnakeCase;
+    }
+    public String getCapital(){
+        return capital;
+    }
+}
---
+++ src/main/java/com/github/Ramble21/commands/StateFlags.java
@@ -0,0 +1,135 @@
+package com.github.Ramble21.commands;
+
+import com.github.Ramble21.classes.State;
+import com.github.Ramble21.command.Command;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.utils.FileUpload;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.io.File;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.concurrent.*;
+
+public class StateFlags implements Command {
+
+    private InteractionHook storedHook;
+    private static int streak = 1;
+    private static User lastUser;
+    private boolean gameIsInProgress;
+    private MessageChannel currentChannel = null;
+    private static StateFlags thisInstance;
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event){
+        if (thisInstance != null){
+            if (thisInstance.gameIsInProgress && event.getChannel() == thisInstance.currentChannel){
+                event.reply("There is already a game going on in this channel!").setEphemeral(true).queue();
+                return;
+            }
+        }
+        User skibidi = event.getUser();
+        String user = skibidi.getId();
+
+        Color red = new Color(255, 0, 0);
+        Color green = new Color(0, 255, 0);
+        Color blue = new Color(0, 122, 255);
+
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle("What US state flag is this?");
+        State state = new State();
+        embed.setColor(blue);
+        System.out.println(state.getName());
+        gameIsInProgress = true;
+        currentChannel = event.getChannel();
+        thisInstance = this;
+
+        String filename = state.getNameSnakeCase() + ".png";
+        File file = new File(("src/main/images/flags/" + filename));
+
+
+
+        embed.setImage("attachment://mystery.png");
+        event.replyEmbeds(embed.build())
+                .addFiles(FileUpload.fromData(file, "mystery.png"))
+                .queue(hook -> {
+                    storedHook = hook;
+                });
+
+        LocalDateTime start = LocalDateTime.now();
+
+
+
+        event.getJDA().addEventListener(new ListenerAdapter() {
+
+            final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+            ScheduledFuture<?> timeoutTask = null;
+
+            @Override
+            public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+                String guess = event.getMessage().getContentRaw().toLowerCase();
+                System.out.println("game is in progress: " + gameIsInProgress);
+
+                timeoutTask = scheduler.schedule(() -> {
+                    if (gameIsInProgress) {
+                        EmbedBuilder timeoutEmbed = new EmbedBuilder();
+                        timeoutEmbed.setTitle("What US state flag is this?");
+                        timeoutEmbed.setDescription("Nobody answered correctly during the time limit, losers");
+                        streak = 0;
+                        lastUser = null;
+                        timeoutEmbed.setColor(red);
+                        timeoutEmbed.setImage("attachment://mystery.png");
+
+                        storedHook.editOriginalEmbeds(timeoutEmbed.build()).queue();
+                        gameIsInProgress = false;
+                        currentChannel = null;
+
+                        event.getJDA().removeEventListener(this);
+                    }
+                }, 10, TimeUnit.SECONDS);
+
+                if (guess.equalsIgnoreCase(state.getName())) {
+                    LocalDateTime end = LocalDateTime.now();
+                    Duration duration = Duration.between(start, end);
+                    double seconds = (double) (duration.toMillis());
+                    String sussyTime = Double.toString(seconds / 1000);
+                    String time = sussyTime.substring(0, sussyTime.length() - 2);
+
+                    EmbedBuilder embed2 = new EmbedBuilder();
+                    embed2.setTitle("What US state flag is this?");
+                    embed2.setDescription("**Answered correctly by <@" + event.getAuthor().getId() + "> in " + time + " seconds!**");
+                    embed2.setColor(green);
+                    embed2.setImage("attachment://mystery.png");
+
+                    storedHook.editOriginalEmbeds(embed2.build()).queue();
+                    event.getJDA().removeEventListener(this);
+
+                    EmbedBuilder embed3 = new EmbedBuilder();
+                    if (lastUser == event.getAuthor()) {
+                        streak++;
+                        embed3.setTitle("Congrats! You guessed correctly! x" + streak);
+                    } else {
+                        embed3.setTitle("Congrats! You guessed correctly!");
+                        lastUser = event.getAuthor();
+                    }
+                    embed3.setDescription("The state was **" + state.getName() + "**");
+                    embed3.setColor(Color.GREEN);
+                    gameIsInProgress = false;
+                    currentChannel = null;
+                    event.getMessage().replyEmbeds(embed3.build()).queue();
+
+                    if (timeoutTask != null && !timeoutTask.isDone()) {
+                        timeoutTask.cancel(false);
+                    }
+                }
+            }
+        });
+    }
+}
