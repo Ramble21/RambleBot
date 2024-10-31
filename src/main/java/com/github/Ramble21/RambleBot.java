e4312cae720004be75a7074509c2ca1d00cba7b8
Ramble21 <zramble21@gmail.com>
2024-10-31 15:33:35 -0400

    important

--- src/main/java/com/github/Ramble21/RambleBot.java
+++ src/main/java/com/github/Ramble21/RambleBot.java
@@ -2,6 +2,7 @@ package com.github.Ramble21;
 
 import com.github.Ramble21.command.CommandListener;
 import com.github.Ramble21.listeners.TextCommand;
+import com.github.Ramble21.listeners.mewhen2;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -40,7 +41,7 @@ public class RambleBot {
         builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
 
         // Register listeners
-        builder.addEventListeners(new CommandListener(), new TextCommand());
+        builder.addEventListeners(new CommandListener(), new TextCommand(), new mewhen2());
 
         // Add jda variable
         jda = JDABuilder.createDefault(token).build();
---
+++ src/main/java/com/github/Ramble21/listeners/mewhen2.java
@@ -0,0 +1,22 @@
+package com.github.Ramble21.listeners;
+
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class mewhen2 extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if (!event.getGuild().getId().equals("931838136223412235")){
+            return;
+        }
+        else if (!event.getChannel().asTextChannel().getName().equalsIgnoreCase("glisterbot-errors")){
+            return;
+        }
+        else if (!event.getAuthor().getId().equals("1029237685656760352")){
+            return;
+        }
+        event.getMessage().addReaction(Emoji.fromCustom("Mewhen2", 940572260421476352L, false)).queue();
+    }
+}
