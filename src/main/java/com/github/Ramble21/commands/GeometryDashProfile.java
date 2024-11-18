deab8cb638ad3f4bfe66bfd2243e83abb50b4e4f
Ramble21 <zramble21@gmail.com>
2024-11-17 21:05:49 -0500

    added likely buggy paginator for /gd profile

--- src/main/java/com/github/Ramble21/commands/GeometryDashProfile.java
+++ src/main/java/com/github/Ramble21/commands/GeometryDashProfile.java
@@ -3,9 +3,12 @@ package com.github.Ramble21.commands;
 import com.github.Ramble21.classes.GeometryDashLevel;
 import com.github.Ramble21.classes.Ramble21;
 import com.github.Ramble21.command.Command;
+import com.github.Ramble21.listeners.GeometryDashReviewButtonListener;
+import com.github.Ramble21.listeners.PaginatorListener;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -13,9 +16,20 @@ import java.util.Objects;
 
 
 public class GeometryDashProfile implements Command {
+
+    private Member member;
+    private String originalMessageId;
+
+    public Member getMember() {
+        return member;
+    }
+    public String getOriginalMessageId() {
+        return originalMessageId;
+    }
+
     @Override
     public void execute(SlashCommandInteractionEvent event){
-        Member member;
+
         if (event.getOption("member") == null){
             member = event.getMember();
         }
@@ -28,23 +42,39 @@ public class GeometryDashProfile implements Command {
         EmbedBuilder embed = new EmbedBuilder();
         embed.setTitle(member.getEffectiveName() + "'s Hardest Completions");
 
-        String description = "";
-
+        String description;
         if (list == null || list.isEmpty()){
             description = member.getAsMention() + " has not submitted any completions yet!";
         }
         else{
             Ramble21.sortByEstimatedDiff(list);
-            for (int i = 0; i < 15 && i < list.size(); i++){
-                String emoji = Ramble21.getEmojiName(list.get(i).getDifficulty());
-                description += (
-                    i+1 + " - " + emoji + " **" + list.get(i).getName() + "** by " + list.get(i).getAuthor() + "\n" +
-                    "<:length:1307507840864227468> *Attempts: " + list.get(i).getAttempts() + "*\n\n"
-                );
-            }
+            description = makePageProfileDescription(list, 15, 0);
         }
+
         embed.setDescription(description);
         embed.setColor(Color.yellow);
-        event.replyEmbeds(embed.build()).queue();
+        event.deferReply().queue(hook -> {
+            hook.sendMessageEmbeds(embed.build())
+                    .addActionRow(
+                        Button.secondary("previous_profile", "Previous"),
+                        Button.secondary("next_profile", "Next"))
+                    .queue(message -> {
+                        this.originalMessageId = message.getId();
+                        PaginatorListener paginatorListener = new PaginatorListener(this, originalMessageId);
+                        event.getJDA().addEventListener(paginatorListener);
+                    });
+        });
+    }
+    public static String makePageProfileDescription(ArrayList<GeometryDashLevel> list, int perPage, int pageNo){
+        Ramble21.sortByEstimatedDiff(list);
+        String description = "";
+        for (int i = pageNo*perPage; i < perPage+(pageNo*perPage) && i < list.size(); i++){
+            String emoji = Ramble21.getEmojiName(list.get(i).getDifficulty());
+            description += (
+                    i+1 + " - " + emoji + " **" + list.get(i).getName() + "** by " + list.get(i).getAuthor() + "\n" +
+                            "<:length:1307507840864227468> *Attempts: " + list.get(i).getAttempts() + "*\n\n"
+            );
+        }
+        return description;
     }
 }
---
+++ src/main/java/com/github/Ramble21/listeners/PaginatorListener.java
@@ -0,0 +1,82 @@
+package com.github.Ramble21.listeners;
+
+import com.github.Ramble21.classes.GeometryDashLevel;
+import com.github.Ramble21.classes.Ramble21;
+import com.github.Ramble21.commands.GeometryDashProfile;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class PaginatorListener extends ListenerAdapter {
+
+    private int pageNo = 0;
+    private GeometryDashProfile gdProfileInstance;
+    private String messageId;
+
+    public PaginatorListener( /* for the future */ ){
+
+    }
+    public PaginatorListener(GeometryDashProfile gdProfileInstance, String messageId){
+        this.gdProfileInstance = gdProfileInstance;
+        this.messageId = messageId;
+    }
+    public PaginatorListener(GeometryDashProfile gdProfileInstance, String messageId, int startingPage){
+        this.gdProfileInstance = gdProfileInstance;
+        this.messageId = messageId;
+        this.pageNo = startingPage;
+    }
+
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent buttonEvent) {
+        if (!(buttonEvent.getMessageId().equals(messageId))){
+            return;
+        }
+        switch(Objects.requireNonNull(buttonEvent.getComponent().getId())) {
+            case "next_profile":
+            {
+                ArrayList<GeometryDashLevel> list = GeometryDashLevel.getPersonalJsonList(gdProfileInstance.getMember().getUser());
+
+                assert list != null;
+                Ramble21.sortByEstimatedDiff(list);
+
+                pageNo++;
+
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+                embedBuilder.setDescription(GeometryDashProfile.makePageProfileDescription(list, 15, pageNo));
+                embedBuilder.setColor(Color.yellow);
+                buttonEvent.editMessageEmbeds(embedBuilder.build()).queue();
+
+                buttonEvent.getJDA().removeEventListener(this);
+                PaginatorListener replacement = new PaginatorListener(gdProfileInstance, messageId, pageNo);
+                buttonEvent.getJDA().addEventListener(replacement);
+                break;
+            }
+            case "previous_profile":
+            {
+                ArrayList<GeometryDashLevel> list = GeometryDashLevel.getPersonalJsonList(gdProfileInstance.getMember().getUser());
+                if (pageNo > 0){
+                    pageNo--;
+                }
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+                embedBuilder.setDescription(GeometryDashProfile.makePageProfileDescription(list, 15, pageNo));
+                embedBuilder.setColor(Color.yellow);
+                buttonEvent.editMessageEmbeds(embedBuilder.build()).queue();
+
+                buttonEvent.getJDA().removeEventListener(this);
+                PaginatorListener replacement = new PaginatorListener(gdProfileInstance, messageId, pageNo);
+                buttonEvent.getJDA().addEventListener(replacement);
+                break;
+            }
+            default:
+            {
+                System.out.println("Unhandled button ID: " + buttonEvent.getComponent().getId());
+                break;
+            }
+        }
+    }
+}
