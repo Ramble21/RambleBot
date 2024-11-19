28d7d44e8c508d4d0a0fd43121802e40919bf25f
Ramble21 <zramble21@gmail.com>
2024-11-19 18:44:03 -0500

    gddl support extended

--- src/main/java/com/github/Ramble21/listeners/Test.java
+++ src/main/java/com/github/Ramble21/listeners/Test.java
@@ -17,20 +17,25 @@ public class Test extends ListenerAdapter {
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if (event.getMessage().getContentRaw().equals("r!append_jsons")){
             String[] filePaths = {
-                    "data/json/completions/470010465486110730.json",
-                    "data/json/completions/597173421671186433.json",
-                    "data/json/completions/597173421671186433.json",
-                    "data/json/completions/674819147963564054.json",
-                    "data/json/completions/681666746511523851.json",
-                    "data/json/completions/739978476651544607.json",
-                    "data/json/completions/759086512586358794.json",
-                    "data/json/completions/786006212415979570.json",
-                    "data/json/completions/840216337119969301.json",
-                    "data/json/completions/917819764682915912.json",
-                    "data/json/completions/987132003331764284.json",
-                    "data/json/completions/1030383982774321162.json",
-                    "data/json/completions/1105270481294209075.json",
-                    "data/json/completions/1206894200831090700.json",
+                    "data/json/completions/classic/364135729737105409.json",
+                    "data/json/completions/classic/470010465486110730.json",
+                    "data/json/completions/classic/597173421671186433.json",
+                    "data/json/completions/classic/615425385231810560.json",
+                    "data/json/completions/classic/674819147963564054.json",
+                    "data/json/completions/classic/681666746511523851.json",
+                    "data/json/completions/classic/730407701057896521.json",
+                    "data/json/completions/classic/739978476651544607.json",
+                    "data/json/completions/classic/759086512586358794.json",
+                    "data/json/completions/classic/786006212415979570.json",
+                    "data/json/completions/classic/840216337119969301.json",
+                    "data/json/completions/classic/917819764682915912.json",
+                    "data/json/completions/classic/987132003331764284.json",
+                    "data/json/completions/classic/1030383982774321162.json",
+                    "data/json/completions/classic/1105270481294209075.json",
+                    "data/json/completions/classic/1206894200831090700.json",
+                    "data/json/completions/platformer/597173421671186433.json",
+                    "data/json/completions/platformer/681666746511523851.json",
+                    "data/json/completions/platformer/739978476651544607.json",
             };
 
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -58,6 +63,10 @@ public class Test extends ListenerAdapter {
                 }
                 System.out.println(filePath.substring(17) + " updated successfully");
             }
+        }
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("r!json")){
+            GeometryDashLevel.initializeGddlMap();
+            System.out.println(GeometryDashLevel.gddlTiers.size());
         }
     }
 }
