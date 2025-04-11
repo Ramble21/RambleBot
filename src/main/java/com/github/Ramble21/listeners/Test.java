bc9c98a505d1b9c724690f35938164f17a4cc140
Ramble21 <zramble21@gmail.com>
2025-04-11 14:34:51 -0400

    ain't nothing but a heartache

--- src/main/java/com/github/Ramble21/listeners/Test.java
+++ src/main/java/com/github/Ramble21/listeners/Test.java
@@ -1,7 +1,5 @@
 package com.github.Ramble21.listeners;
 import com.github.Ramble21.classes.Ramble21;
-import com.github.Ramble21.classes.chess.Board;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -10,10 +8,7 @@ public class Test extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if (event.getMessage().getContentRaw().equals("r!test") && Ramble21.isBotOwner(event.getAuthor())){
-            Board b = new Board();
-            EmbedBuilder e = new EmbedBuilder();
-            e.setDescription(b.toString());
-            event.getChannel().sendMessageEmbeds(e.build()).queue();
+            System.out.println("Hello world!");
         }
     }
 }
