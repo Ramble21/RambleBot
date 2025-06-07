f5e6abc169ecf3c51101ca8ad3463bdbc6476981
Ramble21 <zramble21@gmail.com>
2025-06-07 12:19:58 -0400

    minor rizz rater change

--- src/main/java/com/github/Ramble21/commands/RizzRater.java
+++ src/main/java/com/github/Ramble21/commands/RizzRater.java
@@ -35,12 +35,12 @@ public class RizzRater implements Command {
             case 1 -> "rizz count is **1/10**. I heard they are good friends with that P. Diddy fella, I see them together quite a bit";
             case 2 -> "rizz count is **2/10**. Maybe they should stop gooning to so much anime and try to touch some grass";
             case 3 -> "rizz count is **3/10**. They stare at their crush from a distance instead of actually trying to talk";
-            case 4 -> "rizz count is **4/10**. I would check that out if I were them, before it rapidly gets even worse";
-            case 5 -> "rizz count is **5/10**. Booooooooring";
+            case 4 -> "rizz count is **4/10**. From what I heard, they learned the hard way that chugging an Among Us potion at 3AM wouldn't improve their rizz";
+            case 5 -> "rizz count is **5/10**. The overly racist rhetoric they often spill on first dates repels most, but the ones that do stay love it";
             case 6 -> "rizz count is **6/10**. From my personal experiences with them, I'm honestly shocked that its even this high";
             case 7 -> "rizz count is **7/10**. They only know how to rizz up dudes though, maybe they should try to vary it up";
             case 8 -> "rizz count is **8/10**. With such good looks and smooth talking, it's a shame they constantly scare away their dates with their atrocious body odor";
-            case 9 -> "rizz count is **9/10**. Maybe this is a result of them finally not playing geometry dash anymore";
+            case 9 -> "rizz count is **9/10**. Hanging out with Keanu Reeves and Jack Black has turned them into a rizzler who can rival Big Chungus himself!";
             case 10 -> "rizz count is **10/10**. Holy shit. We have found the skibidi sigma rizzler themself. Maybe even the future CEO of Ohio";
             default -> "rizz count is **-1/12** because there's a bug in this bot's stupid code";
         };
