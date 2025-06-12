bec6a7e4445c7e23d1f3f084bd2431e8874a282f
Ramble21 <zramble21@gmail.com>
2025-06-12 15:28:46 -0400

    main class cleanup

--- src/main/java/com/github/Ramble21/RambleBot.java
+++ src/main/java/com/github/Ramble21/RambleBot.java
@@ -1,6 +1,5 @@
 package com.github.Ramble21;
 
-import com.github.Ramble21.classes.Ramble21;
 import com.github.Ramble21.command.CommandListener;
 import com.github.Ramble21.listeners.*;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -14,23 +13,36 @@ import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
-import javax.security.auth.login.LoginException;
 import java.awt.*;
 import java.io.File;
 
 public class RambleBot {
 
-    private final ShardManager shardManager;
-    private final Dotenv config;
+    private static ShardManager shardManager;
     public static final Color killbotEnjoyer = new Color(174, 221, 0);
     public static final Color scaryOrange = new Color(240, 76, 0);
-    private static JDA jda = null;
-
     private static boolean maintenanceMode;
+    public static boolean validateToken(String token) {
+        try {
+            JDA testJda = JDABuilder.createDefault(token).build();
+            testJda.awaitReady();
+            System.out.println("Token successfully validated " + testJda.getSelfUser().getAsTag());
+            testJda.shutdown();
+            return true;
 
-    public RambleBot() throws LoginException {
-        config = Dotenv.configure().load();
+        } catch (Exception e) {
+            System.out.println("Invalid token or failed to connect: " + e.getMessage());
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+
+        // Load bot token
+        Dotenv config = Dotenv.configure().load();
         String token = config.get("TOKEN");
+        if (!validateToken(token)) {
+            return;
+        }
 
         // Build shard manager
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
@@ -46,17 +58,16 @@ public class RambleBot {
         builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
 
         // Register listeners
-        builder.addEventListeners(new CommandListener(), new TextCommand(), new mewhen2(), new Test(), new HotLavaAndChicken(), new WordCensorListener(), new TheCounter());
+        builder.addEventListeners(
+            new CommandListener(),
+            new TextCommand(),
+            new mewhen2(),
+            new Test(),
+            new HotLavaAndChicken(),
+            new WordCensorListener(),
+            new TheCounter()
+        );
 
-        // Add jda variable
-        jda = JDABuilder.createDefault(token).build();
-        try {
-            jda.awaitReady();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        // Build shard manager
         shardManager = builder.build();
 
         // Declare global variables
@@ -67,7 +78,7 @@ public class RambleBot {
     }
 
     public static JDA getJda() {
-        return jda;
+        return shardManager.getShardById(0);
     }
     public static void setMaintenanceMode(boolean b) {
         maintenanceMode = b;
@@ -76,12 +87,4 @@ public class RambleBot {
         return maintenanceMode;
     }
 
-    public static void main(String[] args) {
-        try {
-            new RambleBot();
-        }
-        catch (LoginException e) {
-            System.out.println("ERROR: Provided bot token is invalid");
-        }
-    }
 }
